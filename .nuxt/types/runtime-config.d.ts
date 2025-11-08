61fdfbff90642497bd1a0bdd0fd9dbcfcbfa7c1a
Avetius <avet.sargsyan@gmail.com>
2025-11-07 21:38:09 -0800

    initial commit

---
+++ .nuxt/types/runtime-config.d.ts
@@ -0,0 +1,94 @@
+import { RuntimeConfig as UserRuntimeConfig, PublicRuntimeConfig as UserPublicRuntimeConfig } from 'nuxt/schema'
+  interface SharedRuntimeConfig {
+   app: {
+      buildId: string,
+
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+
+   nitro: {
+      envPrefix: string,
+   },
+
+   "nuxt-scripts": {
+      version: string,
+   },
+
+   icon: {
+      serverKnownCssClasses: Array<any>,
+   },
+
+   content: {
+      databaseVersion: string,
+
+      version: string,
+
+      database: {
+         type: string,
+
+         filename: string,
+      },
+
+      localDatabase: {
+         type: string,
+
+         filename: string,
+      },
+
+      integrityCheck: boolean,
+   },
+  }
+  interface SharedPublicRuntimeConfig {
+   mdc: {
+      components: {
+         prose: boolean,
+
+         map: any,
+      },
+
+      headings: {
+         anchorLinks: {
+            h1: boolean,
+
+            h2: boolean,
+
+            h3: boolean,
+
+            h4: boolean,
+
+            h5: boolean,
+
+            h6: boolean,
+         },
+      },
+   },
+
+   "nuxt-scripts": {
+      version: any,
+
+      defaultScriptOptions: {
+         trigger: string,
+      },
+   },
+
+   content: {
+      wsUrl: string,
+   },
+  }
+declare module '@nuxt/schema' {
+  interface RuntimeConfig extends UserRuntimeConfig {}
+  interface PublicRuntimeConfig extends UserPublicRuntimeConfig {}
+}
+declare module 'nuxt/schema' {
+  interface RuntimeConfig extends SharedRuntimeConfig {}
+  interface PublicRuntimeConfig extends SharedPublicRuntimeConfig {}
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: UserRuntimeConfig
+        }
+      }
